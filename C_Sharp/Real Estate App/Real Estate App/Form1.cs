c22a192eafe8c21ded9f474e9acaa57d0e224bbf
Hamzah <hamza.abdo19@gmail.com>
2022-08-09 17:54:07 +0300

    Login part

--- C_Sharp/Real Estate App/Real Estate App/Form1.cs
+++ C_Sharp/Real Estate App/Real Estate App/Form1.cs
@@ -16,5 +16,20 @@ namespace Real_Estate_App
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
