995130e4f13c3854dcc54046a8238e78d00b6659
Kidanekal <akidanekal@gmail.com>
2022-08-10 01:12:59 +0900

    Init property class

---
+++ C_Sharp/Real Estate App/Real Estate App/Model/Property.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_Estate_App.Model
+{
+    internal class Property
+    {
+        public string Id { get; set; }
+        public string Address { get; set; }
+        public Image Photo { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+        public float RentPrice { get; set; }
+        public float SellPrice { get; set; }
+        /***
+         * Update a property in the database table
+         */
+        public bool Update()
+        {
+
+        }
+        /***
+         * Delete a property from database table
+         */
+        public bool Delete()
+        {
+
+        }
+        /***
+         * Add a property to the database table 
+         */
+        public bool Add()
+        {
+
+        }
+        /***
+         * get all properties
+         */
+        public List<Property> GetAllProperties()
+        {
+
+        }
+        /***
+           * get all available properties
+          */
+        public List<Property> GetAllAvailableProperties()
+        {
+
+        }
+        /***
+         * Filter properties by address
+         * type
+         * sell price or rent price 
+         */
+        public List<Property> FilterProperties(string address, string type,string status, float sellPrice, float rentPRice)
+        {
+
+        }
+        /***
+         * Get all properties that were sold 
+         */
+        public List<Property> GetAllSoldProperties()
+        {
+
+        }
+        /***
+         * Get all properties that were rented
+         */
+        public List<Property> GetRentedProperties()
+        {
+
+        }
+    }
+}
