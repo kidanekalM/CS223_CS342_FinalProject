161c398739c05c306973861ff6aa1d78429f0848
Hamzah <hamza.abdo19@gmail.com>
2022-10-19 19:24:59 +0300

    staged

---
+++ C_Sharp/Real Estate App/Real Estate App/Admin Pages/AddAppointment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Real_Estate_App.Admin_Pages
+{
+    public partial class AddAppointment : Form
+    {
+        public AddAppointment()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
